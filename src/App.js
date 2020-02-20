0a75889b2cc1b6c981afae0808000a7094e618c1
thomasng <thomas.ng@isuntv-hk.com>
2020-02-21 00:13:36 +0800

    completed question 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import Modal from "./Components/Modal";
+import Modali, { useModali } from 'modali';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [count , setCount] = useState()
+  const [modalConfig, openModal] = useModali({
+    animated: true,
+    large: true,
+    closeButton: false,
+    centered: true
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"App"}>
+      <Button onClick={openModal} >Open</Button>
+      <Modali.Modal {...modalConfig}>
+        <div className="container">
+        </div>
+      </Modali.Modal>
     </div>
   );
-}
+};
+
 
 export default App;
